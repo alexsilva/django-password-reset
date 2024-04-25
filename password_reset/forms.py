174f9a3da3a6d5b07a4b9ed01415b677b3507897
alexsilva <geniofuturo@gmail.com>
2024-04-25 17:18:57 -0300

    Replace 'ugettext_lazy' with 'gettext_lazy'.

--- password_reset/forms.py
+++ password_reset/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ImproperlyConfigured
 from django.core.validators import validate_email
 from django.db.models import Q
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from .compat import get_user_model
 
